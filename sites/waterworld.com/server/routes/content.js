33a7fb88ff3ca43b7c58dd1162f76a774cd36b87
Jacob Bare <jacob@limit0.io>
2020-02-19 16:37:35 -0600

    Port leaders company layout and styling

--- sites/waterworld.com/server/routes/content.js
+++ sites/waterworld.com/server/routes/content.js
@@ -3,6 +3,7 @@ const content = require('../templates/content');
 const company = require('../templates/content/company');
 const whitepaper = require('../templates/content/whitepaper');
 const queryFragment = require('../graphql/fragments/content-page');
+const companyQueryFragment = require('../graphql/fragments/content-company');
 
 module.exports = (app) => {
   app.get('/*?whitepaper/:id(\\d{8})*', withContent({
@@ -11,7 +12,7 @@ module.exports = (app) => {
   }));
   app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
-    queryFragment,
+    queryFragment: companyQueryFragment,
   }));
   app.get('/*?:id(\\d{8})*', withContent({
     template: content,
