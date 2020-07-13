6e733d9ddbf398bbcddc072cd46bdad12e7352fc
Brian Miller <bmiller@southcomm.com>
2020-07-13 07:51:34 -0500

    remove contact us form common components

--- packages/common/browser/index.js
+++ packages/common/browser/index.js
@@ -1,11 +1,9 @@
-import ContactUsForm from './contact-us-form.vue';
 import AutoScroll from './auto-scroll.vue';
 
 const AlgoliaSiteSearch = () => import(/* webpackChunkName: "common-algolia-site-search" */ './algolia/site-search.vue');
 
 export default (Browser) => {
   // @todo this should be removed once contact us is moved to core.
-  Browser.registerComponent('CommonContactUsForm', ContactUsForm);
   Browser.registerComponent('CommonAutoScroll', AutoScroll);
   Browser.registerComponent('CommonAlgoliaSiteSearch', AlgoliaSiteSearch);
 };
