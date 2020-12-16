5553f8133c2b1f078b19cd0dc39d5506227c2cd4
Alysha Buselt <Alixers505@users.noreply.github.com>
2020-12-16 11:35:45 -0500

    Updated navs and logo for VSP
    Per https://southcomm.atlassian.net/browse/DEV-410

--- sites/vehicleservicepros.com/config/site.js
+++ sites/vehicleservicepros.com/config/site.js
@@ -1,8 +1,8 @@
 const navigation = require('./navigation');
 const gam = require('./gam');
-
 const nativeX = require('./native-x');
 const dragonForms = require('./dragon-forms');
+const siteLogo = 'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/vsp.png?h=60';
 
 module.exports = {
   navigation,
@@ -12,15 +12,15 @@ module.exports = {
   company: 'Endeavor Business Media, LLC',
   logos: {
     navbar: {
-      src: 'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/VSP_header_logos_V2.png?h=60',
+      src: siteLogo,
       srcset: [
-        'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/VSP_header_logos_V2.png?h=120 2x',
+        'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/vsp.png?h=120 2x',
       ],
     },
     footer: {
-      src: 'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/VSP_header_logos_V2.png?h=60',
+      src: siteLogo,
       srcset: [
-        'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/VSP_header_logos_V2.png?h=120 2x',
+        'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/vsp.png?h=120 2x',
       ],
     },
   },
@@ -59,7 +59,7 @@ module.exports = {
   contactUs: {
     branding: {
       bgColor: '#164f77',
-      logo: 'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/vsp.png?h=60',
+      logo: siteLogo,
     },
     to: 'contact@vehicleservicepros.com',
   },
@@ -69,7 +69,7 @@ module.exports = {
     sendTo: 'sales@vehicleservicepros.com',
     sendFrom: 'VehicleServicePros.com <noreply@baseplatform.io>',
     sendBcc: 'eactivity@endeavorb2b.com',
-    logo: 'https://img.vehicleservicepros.com/files/base/cygnus/vspc/image/static/logo/vsp.png?h=60',
+    logo: siteLogo,
     bgColor: '#164f77',
   },
 };
