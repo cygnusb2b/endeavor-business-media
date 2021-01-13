f5feb28ff734c0106354d67ff7e9e1b1bba33d2d
Alysha Buselt <Alixers505@users.noreply.github.com>
2021-01-13 15:52:50 -0500

    Added native-x ids for new topics

--- sites/vehicleservicepros.com/config/native-x.js
+++ sites/vehicleservicepros.com/config/native-x.js
@@ -13,16 +13,16 @@ config
     { name: 'default', id: '5b57587e5bbf91000134a2c2' },
   ])
   .setAliasPlacements('collision-repair', [
-    { name: 'default', id: '' },
+    { name: 'default', id: '5fff5cb7ba54220001f6c3b7' },
   ])
   .setAliasPlacements('service-repair', [
-    { name: 'default', id: '' },
+    { name: 'default', id: '5fff5cf18e900f00013244c8' },
   ])
   .setAliasPlacements('distribution', [
-    { name: 'default', id: '' },
+    { name: 'default', id: '5fff5d0ce460e00001537aa3' },
   ])
   .setAliasPlacements('training-and-resources', [
-    { name: 'default', id: '' },
+    { name: 'default', id: '5fff5d2801260d0001041a79' },
   ]);
 
 module.exports = config;
